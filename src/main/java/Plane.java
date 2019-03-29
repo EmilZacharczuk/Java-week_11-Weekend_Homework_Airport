b25d617e093127808766311fe247d6649dc4c6c7
EmilZacharczuk <emil.zacharczuk@yahoo.pl>
2019-03-29 16:37:36 +0000

    Plane tests done

--- src/main/java/Plane.java
+++ src/main/java/Plane.java
@@ -9,10 +9,24 @@ public class Plane {
     public Plane(PlaneType planeType, String airline) {
         this.planeType = planeType;
         this.airline = airline;
-        this.passengers = new ArrayList<Passenger>();
+        this.passengers = new ArrayList<>();
     }
 
     public PlaneType getPlaneType() {
         return this.planeType;
+    }
+
+    public String getAirline() {
+        return this.airline;
+    }
+    public void addPassenger(Passenger passenger) {
+        this.passengers.add(passenger);
+    }
+
+    public int passengersCount() {
+        return this.passengers.size();
+    }
+    public Passenger removePassenger() {
+        return this.passengers.remove(0);
     }
 }
--- src/test/java/PlaneTest.java
+++ src/test/java/PlaneTest.java
@@ -1,4 +1,45 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class PlaneTest {
 
+    Plane plane;
+    Passenger passenger;
+
+    @Before
+    public void before() {
+        this.plane = new Plane(PlaneType.PIPER, "EasyJet");
+    }
+
+    @Test
+    public void haveType() {
+        assertEquals(PlaneType.PIPER, plane.getPlaneType());
+    }
+
+    @Test
+    public void haveAirline() {
+        assertEquals("EasyJet", plane.getAirline());
+    }
+
+    @Test
+    public void canAddPassengers() {
+        passenger = new Passenger("Johny Walker");
+        plane.addPassenger(passenger);
+        assertEquals(1, this.plane.passengersCount());
+    }
+    @Test
+    public void canHavePassengrs() {
+        assertEquals(0, this.plane.passengersCount());
+    }
+    @Test
+    public void canRemovePassengers() {
+        passenger = new Passenger("Johny Walker");
+        plane.addPassenger(passenger);
+        plane.addPassenger(passenger);
+        plane.removePassenger();
+        assertEquals(1, this.plane.passengersCount());
+    }
 
 }
